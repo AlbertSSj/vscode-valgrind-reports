79f5653dff42674da66aede7fe364b80c4613a34
AlbertSSj <alberto.ing@gmail.com>
2020-04-08 18:45:41 +0200

    Registered a basic text provider

--- src/extension.ts
+++ src/extension.ts
@@ -2,119 +2,50 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+let logger: vscode.OutputChannel | undefined = undefined;
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "valgrind-reports" is now active!');
+	// Initialise the logger
+	logger = vscode.window.createOutputChannel("Valgrind Reports");
+
+	// Register the document provider
+	let provider = new ValgrindReportsProvider();
+	context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider('valgrind', provider));
 
 	// The command to open the report windows
-	context.subscriptions.push(vscode.commands.registerCommand('valgrindReports.openReports', () => {
-		// Create or show panel
-		ValgrindReportsPanel.createOrShow();
-	}));
+	context.subscriptions.push(vscode.commands.registerCommand('valgrindReports.openReports', command_openReports));
 
 	// The command to add a file to the report list
-	context.subscriptions.push(vscode.commands.registerCommand('valgrindReports.addFile', () => {
-		addFileToConfig();
-	}));
+	context.subscriptions.push(vscode.commands.registerCommand('valgrindReports.addFile', command_addFile));
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() { }
-
-class ValgrindReportsPanel {
-	public static readonly viewType = 'valgrindReports';
-	public static readonly log = vscode.window.createOutputChannel("Valgrind Reports");
-
-	private static self: ValgrindReportsPanel;
-
-	private readonly _panel: vscode.WebviewPanel;
-	private readonly _disposables: vscode.Disposable[] = [];
-
-	public static createOrShow() {
-		const column = vscode.window.activeTextEditor
-			? vscode.window.activeTextEditor.viewColumn
-			: undefined;
-
-		// If we already have a panel, show it.
-		if (ValgrindReportsPanel.self) {
-			ValgrindReportsPanel.self._panel.reveal(column);
-			return;
-		}
-
-		// Otherwise, create a new panel.
-		const panel = vscode.window.createWebviewPanel(
-			ValgrindReportsPanel.viewType,
-			'Valgrind Reports',
-			column || vscode.ViewColumn.One,
-			{
-				// Enable javascript in the webview
-				enableScripts: false,
-			}
-		);
-
-		ValgrindReportsPanel.self = new ValgrindReportsPanel(panel);
-	}
-
-	private constructor(panel: vscode.WebviewPanel) {
-		this._panel = panel;
-
-		// Set the webview's initial html content
-		this._update();
-
-		// Listen for when the panel is disposed
-		// This happens when the user closes the panel or when the panel is closed programatically
-		this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
-
-		// Update the content based on view changes
-		this._panel.onDidChangeViewState(
-			e => {
-				if (this._panel.visible) {
-					this._update();
-				}
-			},
-			null,
-			this._disposables
-		);
-
-		// Handle messages from the webview
-		this._panel.webview.onDidReceiveMessage(
-			message => {
-				switch (message.command) {
-					case 'alert':
-						vscode.window.showErrorMessage(message.text);
-						return;
-				}
-			},
-			null,
-			this._disposables
-		);
-	}
-
-	private _update() {
-		const webview = this._panel.webview;
-		webview.html = `
-			<!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Valgrind Reports</title>
-            </head>
-            <body>
-				<h1>XXX</h1>
-				<a href='file:///D:/1_Development/testfolder/aaaa.txt'>aaaa.txt</a>
-            </body>
-            </html>`;
-	}
-
-	public dispose() { }
+export function deactivate() {
+	logger = undefined;
 }
 
-async function addFileToConfig() {
+class ValgrindReportsProvider implements vscode.TextDocumentContentProvider {
+	private onDidChangeEmitter = new vscode.EventEmitter<vscode.Uri>();
+	public onDidChange = this.onDidChangeEmitter.event;
+
+	provideTextDocumentContent(uri: vscode.Uri, token: vscode.CancellationToken): vscode.ProviderResult<string> {
+		// Get list from configuration
+		let valgrindReportsCfg = vscode.workspace.getConfiguration('valgrindReports', null);
+		let filesToOpen = valgrindReportsCfg.get<Array<string>>('filesToOpen');
+
+		let result: Array<string> = [];
+		filesToOpen?.forEach(file => {
+			result.push(file);
+		});
+
+		return result.join('\n');
+	}
+}
+
+async function command_addFile() {
 	const input = await vscode.window.showInputBox({ prompt: 'Add Valgrind Report to parse.' });
 	if (input !== undefined) {
 
@@ -129,7 +60,15 @@ async function addFileToConfig() {
 		currentStatus.push(input);
 		valgrindReportsCfg.update('filesToOpen', currentStatus, false, false);
 
-
-		ValgrindReportsPanel.log.appendLine(`Added file ${input} to scanned reports`);
+		logger?.appendLine(`Added file ${input} to scanned reports`);
 	}
 }
+
+async function command_openReports() {
+	// We only use one URI
+	let uri = vscode.Uri.parse('valgrind:reports');
+	// Open the document
+	let doc = await vscode.workspace.openTextDocument(uri);
+	// Show the document
+	await vscode.window.showTextDocument(doc, { preview: false });
+};
